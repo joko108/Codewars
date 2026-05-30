c8a5e2e1ea301350c129313a015204c1d2200385
KirillZverev <zverev04@gmail.com>
2026-05-30 19:55:48 +0300

    combat

--- 8kyu/game-combat-function.js
+++ 8kyu/game-combat-function.js
@@ -1,7 +1,7 @@
 // Create a combat function that takes the 
 // player's current health and the amount of 
 // damage received, and returns the player's new health. 
-// Health can't be less than 0.
+// Health can't be less than 0!
 
 function combat(health, damage) {
     health -= damage;
