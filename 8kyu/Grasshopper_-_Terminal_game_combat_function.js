bd6dfe2b1b1b3939199d6612c92618f32b2d4b1b
KirillZverev <zverev04@gmail.com>
2026-05-31 08:48:14 +0300

    4th, 5th rename

---
+++ 8kyu/Grasshopper_-_Terminal_game_combat_function.js
@@ -0,0 +1,11 @@
+// Create a combat function that takes the 
+// player's current health and the amount of 
+// damage received, and returns the player's new health. 
+// Health can't be less than 0!
+
+function combat(health, damage) {
+    health -= damage;
+    return health > 0 ? health : 0;
+}
+
+console.log(combat(100, 50));
